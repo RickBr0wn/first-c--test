7e8f34fc51ed12be49aadf33153b16d061fb6301
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-22 01:59:21 +0000

    Namespaces

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
-class Program
-{   
-    static void Main(){
-        // Prompt the user for minutes exercised
-        // Add minutes exercised to total
-        // Display total minutes exercised to the screen
-        // Repeat until the user quits
+namespace TreeHouse {
+
+    class Program {
+        static void Main () {
+
+            // Prompt the user for minutes exercised
+            System.Console.Write ("Enter how many minutes have you exercised: ");
+
+            string entry = System.Console.ReadLine ();
+
+            // Add minutes exercised to total
+            int total = entry;
+
+            // Display total minutes exercised to the screen
+
+            // Repeat until the user quits
+        }
     }
 }
